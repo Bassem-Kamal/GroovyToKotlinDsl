e5e68c5ae107a105ffdca1d94dc3892f7367de44
Bassem Kamal <dev.bassem.kamal@gmail.com>
2021-12-27 11:18:47 +0200

    Add Depends object and use it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,41 +35,27 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-
-    implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-
-    // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    // LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    // Room
-    implementation("androidx.room:room-runtime:2.4.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    kapt("androidx.room:room-compiler:2.4.0")
-    implementation("androidx.room:room-ktx:2.4.0") // Kotlin Extensions and Coroutines support for Room
-    //datastore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    // Activity KTX for viewModels()
-    implementation("androidx.activity:activity-ktx:1.4.0")
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation(Depends.ANDROIDX_CORE)
+    implementation(Depends.APPCOMPAT)
+    implementation(Depends.MATERIAL_DESIGN)
+    implementation(Depends.PICASSO)
+    implementation(Depends.CIRCLE_IMAGEVIEW)
+    implementation(Depends.VIEW_PAGER_2)
+    implementation(Depends.COROUTINES_CORE)
+    implementation(Depends.COROUTINES_ANDROID)
+    implementation(Depends.VIEW_MODEL)
+    implementation(Depends.LIVEDATA)
+    implementation(Depends.NAVIGATION_FRAGMENT)
+    implementation(Depends.NAVIGATION_UI)
+    implementation(Depends.ROOM_RUNTIME)
+    implementation(Depends.ROOM_COMPILER)
+    implementation(Depends.ROOM_KTX)
+    implementation(Depends.LEGACY_SUPPORT_V4)
+    implementation(Depends.DATA_STORE)
+    implementation(Depends.ACTIVITY_KTX)
+    implementation(Depends.RETROFIT_2)
+    implementation(Depends.RETROFIT_2_CONVERTER_GSON)
+    implementation(Depends.RETROFIT_2_COROUTINES_ADAPTER)
 
     testImplementation(TestDepends.JUNIT)
     androidTestImplementation(TestDepends.ANDROIDX_TEST_JUNIT)
@@ -106,6 +92,31 @@ object Versions {
     const val JUNIT = "4.13.2"
     const val ANDROIDX_TEST_JUNIT = "1.1.3"
     const val ANDROIDX_TEST_ESPRESSO = "3.4.0"
+}
+
+object Depends {
+    const val ANDROIDX_CORE = "androidx.core:core-ktx:${Versions.ANDROIDX_CORE}"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
+    const val MATERIAL_DESIGN = "com.google.android.material:material:${Versions.MATERIAL_DESIGN}"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+    const val PICASSO = "com.squareup.picasso:picasso:${Versions.PICASSO}"
+    const val CIRCLE_IMAGEVIEW ="de.hdodenhof:circleimageview:${Versions.CIRCLE_IMAGEVIEW}"
+    const val VIEW_PAGER_2 = "androidx.viewpager2:viewpager2:${Versions.VIEW_PAGER_2}"
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}"
+    const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES}"
+    const val VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.VIEW_MODEL}"
+    const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIVEDATA}"
+    const val NAVIGATION_FRAGMENT = "ndroidx.navigation:navigation-fragment-ktx:${Versions.NAVIGATION}"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Versions.NAVIGATION}"
+    const val ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.ROOM}"
+    const val ROOM_COMPILER = "androidx.room:room-compiler:${Versions.ROOM}"
+    const val ROOM_KTX = "androidx.room:room-ktx:${Versions.ROOM}"
+    const val LEGACY_SUPPORT_V4 = "androidx.legacy:legacy-support-v4:${Versions.LEGACY_SUPPORT_V4}"
+    const val DATA_STORE = "androidx.datastore:datastore-preferences:${Versions.DATA_STORE}"
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
+    const val RETROFIT_2 ="com.squareup.retrofit2:retrofit:${Versions.RETROFIT_2}"
+    const val RETROFIT_2_CONVERTER_GSON ="com.squareup.retrofit2:converter-gson:${Versions.RETROFIT_2}"
+    const val RETROFIT_2_COROUTINES_ADAPTER = "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.CONSTRAINT_LAYOUT}"
 }
 
 object TestDepends {
